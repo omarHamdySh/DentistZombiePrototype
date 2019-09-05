8fd554d79802ec0fa62bee1df08ab2154fbb7228
Omar Hamdy <omar.h.shafeek@gmail.com>
2019-09-05 12:13:30 +0200

    scene 3 updated for timeline events

--- Assets/_Complete-Game/Scripts/UI/SwitchEvent.cs
+++ Assets/_Complete-Game/Scripts/UI/SwitchEvent.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.Video;
 public class SwitchEvent : MonoBehaviour
 {
    
     bool switchFlag = true;         //a flag to switch from pause to resume and the inverse also
     public Text pauseButtonText;    //reference for text field of the button of pause to switch the mode from pause to resume and the opposite also   
-
+    public VideoPlayer video;
     /// <summary>
     /// a functiond to switch the mode from pause mode to resume mode and the opposite 
     /// also passed in the inspectore in interactable gameobject events scipt
@@ -36,6 +36,7 @@ public class SwitchEvent : MonoBehaviour
     public void StartGame()
     {
         //GameManager.Instance.gameplayFSMManager.ChangeToWashing();
+        video.Play();
         TutorialManager.Instance.PlayNextSequence();
     }
 }
